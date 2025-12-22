2a852c81d24be9bbfc03fe257ae7d3f76328ca32
ptm-dotcom <ptm@fohp.com.au>
2025-12-22 17:36:38 +1100

    Update tailwind.config.js

--- avriskassement/tailwind.config.js
+++ avriskassement/tailwind.config.js
@@ -8,14 +8,3 @@ module.exports = {
   },
   plugins: [],
 }
-
-// ============================================
-
-// FILE 6: postcss.config.js
-// Save this as postcss.config.js
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
